acae40787584d85413ac85b4c96068761ad40ae3
littlesDE <s.lueck33@gmail.com>
2025-10-21 11:36:58 +0200

    feat: add header title to Todo App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
     <main className="bg-gray-800 min-w-screen min-h-screen flex flex-col items-center justify-center text-gray-100">
+    <h1 className="text-4xl mb-8">Todo App</h1>
     <Todo />
     </main>
     </>
